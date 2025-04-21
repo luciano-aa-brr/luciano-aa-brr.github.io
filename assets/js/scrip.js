606c6ff51a9fc61f192d1e2eb923e6a525042590
luciano_aa_brr <lucianoaliaga02@gmail.com>
2025-04-20 21:48:12 -0400

    mensaje-bienvenida

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -4,9 +4,24 @@ let contador = localStorage.getItem('contador-visitas');
 if (!contador) {
     contador = 0;
 }
-
 contador++;
 
 localStorage.setItem('contador-visitas', contador);
-
 document.getElementById('contador').innerText = contador;
+
+//mensaje personalizado 
+const ahora = new Date();
+const hora = ahora.getHours();
+let mensaje;
+
+if (hora >= 5 && hora < 12) {
+    mensaje = "¡Buenos días!";
+} else if (hora >= 12 && hora < 18) {
+    mensaje = "¡Buenas tardes!";
+} else if (hora >= 18 && hora < 22) {
+    mensaje = "¡Buenas noches!";
+} else {
+    mensaje = "Es tarde, pero gracias por estar aquí. ";
+}
+
+document.getElementById('mensaje-bienvenida').innerText = mensaje;
